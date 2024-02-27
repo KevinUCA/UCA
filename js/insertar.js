27763759a6849ac1d0837e3f32c95b4faeddb3a8
KevinUCA <156842188+KevinUCA@users.noreply.github.com>
2024-02-27 12:18:23 -0600

    insertar botones

---
+++ js/insertar.js
@@ -0,0 +1,18 @@
+function cambiarContenido(){
+    var div = document.getElementById("miDiv");
+    div.innerHTML ="<h1>Nuevo titulo hecho con innerHTML</h1><p>Contenido nuevo con innerHTML</p>;
+}
+
+function cambiarContenido2(){
+    var div = document.getElementById("miDiv");
+}
+
+var nuevoTexto = document.createElement("h1");
+nuevoTexto.textContent("Titulo nuevo 2 hecho con createTextNode");
+
+div.appendChild(nuevoTexto);
+
+var Parrafo = document.createElement("p");
+Parrafo.textContent = "Contenido nuevo con createTextNode";
+
+div.appendChild(Parrafo);
