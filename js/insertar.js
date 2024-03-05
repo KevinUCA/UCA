b51f35bbe6aeff73848ba318fb40d53be2ff5b21
KevinUCA <156842188+KevinUCA@users.noreply.github.com>
2024-03-05 11:52:55 -0600

    corregir insertar

--- js/insertar.js
+++ js/insertar.js
@@ -1,17 +1,18 @@
 function cambiarContenido(){
     var div = document.getElementById("miDiv");
     div.innerHTML ="<h1>Nuevo titulo hecho con innerHTML</h1><p>Contenido nuevo con innerHTML</p>";
+
+    function cambiarContenido2(){
+        var div = document.getElementById("miDiv");
+    }
+    
+    var nuevoTexto = document.createTextNode("Titulo Nuevo 2 hecho con createTextNode");
+    
+    div.appendChild(nuevoTexto);
+    
+    var Parrafo = document.createElement("p");
+    Parrafo.textContent = "Contenido nuevo con createTextNode";
+    
+    div.appendChild(Parrafo);
+
 }
-
-function cambiarContenido2(){
-    var div = document.getElementById("miDiv");
-}
-
-var nuevoTexto = document.createTextNode("Titulo Nuevo 2 hecho con createTextNode");
-
-div.appendChild(nuevoTexto);
-
-var Parrafo = document.createElement("p");
-Parrafo.textContent = "Contenido nuevo con createTextNode";
-
-div.appendChild(Parrafo);
